4e36a39995854f8f4e3f40126eff5d6bc86c0401
Sam Goodger <samgoodger@ocular.co.nz>
2018-04-09 13:07:17 +1200

    Add comments

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
+// Load dependencies
 const Discord = require('discord.js');
 const latinize = require('latinize');
-const client = new Discord.Client();
+
+// Load config
 const config = require('./config.json');
 
+// Create alot bot
+const client = new Discord.Client();
+
+// Log to console when alot bot is ready
 client.on('ready', () => {
 	console.log('alot is ready and waiting');
 });
 
 client.on('message', (msg) => {
-	if (latinize(msg.content).toLowerCase().includes('alot') || (latinize(msg.content).toLowerCase().includes('a lot') && msg.author.id == config.knightmanId) || msg.isMentioned(client.user)) {
-		try  {
+	// Check messages for certain ways "alot" is said
+	if (
+		// latinize makes sure any diacritics are converted to regular latin characters
+		latinize(msg.content).toLowerCase().includes('alot')
+		// Knightman can't escape the bot
+		|| (latinize(msg.content).toLowerCase().includes('a lot') && msg.author.id == config.knightmanId)
+		// alot is self-aware (because otherwise it won't react to mention of its own name, stupid alot)
+		|| msg.isMentioned(client.user)
+	) {
+		try {
+			// add the reaction
 			msg.react(msg.guild.emojis.get(config.alotEmojiId));
+			// log it to the console
 			console.log(`Reacted: "${msg.content}" by ${msg.author.username}`);
 		} catch (error) {
+			// idk how to error
 			console.error('alot failed');
 		}
 	}
 });
 
+// login boi
 client.login(config.token);
